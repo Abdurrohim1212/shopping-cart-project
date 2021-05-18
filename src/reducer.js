b2f5d430e5593c86930225dc8945098bd151f357
Abdurrohim_ethan <abdurrohim48@gmail.com>
2021-05-18 12:04:36 +0700

    adding reducer functionality of increase and decrease (reducer.js)

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,7 @@
 const reducer = (state, action) => {
   const { type, payload } = action;
   const { cart } = state;
+
   switch (type) {
     case "CLEAR_CART":
       return {
@@ -14,7 +15,26 @@ const reducer = (state, action) => {
           return cartItem.id !== payload;
         }),
       };
-
+    case "INCREASE":
+      return {
+        ...state,
+        cart: cart.map(item => {
+          if (item.id === payload) {
+            return { ...item, amount: item.amount + 1 };
+          }
+          return item;
+        }),
+      };
+    case "DECREASE":
+      return {
+        ...state,
+        cart: cart.map(item => {
+          if (item.id === payload) {
+            return { ...item, amount: item.amount - 1 };
+          }
+          return item;
+        }),
+      };
     default:
       return state;
   }
