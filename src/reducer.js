06461b3f9c93e86659439d44d06e7accbf816d3c
Abdurrohim_ethan <abdurrohim48@gmail.com>
2021-05-18 10:57:37 +0700

    adding functionality reducer of remive item (reducer.js)

--- src/reducer.js
+++ src/reducer.js
@@ -1,10 +1,18 @@
 const reducer = (state, action) => {
-  const { type } = action;
+  const { type, payload } = action;
+
   switch (type) {
     case "CLEAR_CART":
       return {
         ...state,
         cart: [],
+      };
+    case "REMOVE":
+      return {
+        ...state,
+        cart: state.cart.filter(cartItem => {
+          return cartItem.id !== payload;
+        }),
       };
 
     default:
